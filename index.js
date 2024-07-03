9d25ad22601198c69beee1f9a054c46c5a102228
Harsh <hk755063@gmail.com>
2024-07-03 11:23:01 +0530

    custom middleWare

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const app = express();
 app.use(express.json());
+app.use(middleware);
 
 let courses = [
   { id: 1, name: "java" },
@@ -23,7 +24,7 @@ app.put("/courses/:id", (req, res) => {
         res.status(404).send("Course not found");
     } else{
         let course = {
-            id : req.params.id,
+            id : Number(req.params.id),
             name : req.body.name
         }
         courses[req.params.id - 1] = course;
@@ -48,4 +49,20 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+function middleware(req, res, next){
+    const method = req.method;
+    const ip = req.ip;
+    const hostname = req.hostname;
+    const date = new Date();
+
+    console.log(`Method: ${method}`);
+    console.log(`IP: ${ip}`);
+    console.log(`localhost: ${hostname}`);
+    console.log(`Date: ${date}`);
+    next();
+}
+
+const port = 3000;
+
 app.listen(3000, () => console.log("Listening on port 3000..."));
+
