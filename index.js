2d2542893f1da5a72936a1e78acc62a9d5894a22
Harsh <hk755063@gmail.com>
2024-07-03 10:26:28 +0530

    post req

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 let courses = [
-    { id: 1, name: 'java' },
-    { id: 2, name: 'javascript' },
-    { id: 3, name: 'python' }
+  { id: 1, name: "java" },
+  { id: 2, name: "javascript" },
+  { id: 3, name: "python" },
 ];
 
-app.get('/courses', (req, res) => {
-    res.json(courses);
+app.post("/courses", (req, res) => {
+  let singleCourse = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(singleCourse);
+  res.send(singleCourse);
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+// app.put('/courses', (req, res) => {
+//     courses[0].name = 'java1';
+// });
+
+// console.log(courses);
+
+app.get("/courses", (req, res) => {
+  res.json(courses);
 });
 
-app.listen(3000, () => console.log('Listening on port 3000...'));
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => console.log("Listening on port 3000..."));
